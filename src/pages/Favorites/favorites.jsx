f1cecf747a25153450f5618e76fdf8dc9f45b2b7
Connor Gunter <bigc5674@gmail.com>
2024-01-22 09:18:49 -0500

    conditionally rendered login button on favorites page

--- src/pages/Favorites/favorites.jsx
+++ src/pages/Favorites/favorites.jsx
@@ -3,6 +3,7 @@ import { viewFavoriteLocation } from "../../utilities/weather-service";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./favorites.css"
 import { useParams, useNavigate, Link } from "react-router-dom";
+import LoginButton from "../../components/Auth/LoginButton";
 
 
 const Favorites = () => {
@@ -26,27 +27,33 @@ const Favorites = () => {
 
   return (
     <>
-    <h1 className="fav-header">Saved Locations</h1>
-    {userFavorites === null ? (
-      <h1>Loading...</h1>
-    ) : userFavorites.length > 0 ? (
-      <div className="grid-container">
-        {userFavorites[0]?.favorites.map((favorite, index) => (
-          <div key={index} className="fav-loc-card">
-            <Link className="details-link" to={`/favorites/${favorite._id}`}>
-              <h1>{favorite.name}</h1>
-              <h2>{favorite.locationData.weather.current.temp_f}°F</h2>
-              <h2>Wind Speed: {favorite.locationData.weather.current.wind_mph} MPH</h2>
-              <h2>Humidity: {favorite.locationData.weather.current.humidity}%</h2>
-            </Link>
-          </div>
-        ))}
-      </div>
-    ) : (
-      <h1>Loading...</h1>
-    )}
-  </>
-);
+      {!isAuthenticated ? (
+        <LoginButton />
+      ) : (
+        <>
+          <h1 className="fav-header">Saved Locations</h1>
+          {userFavorites === null ? (
+            <h1>Loading...</h1>
+          ) : userFavorites.length > 0 ? (
+            <div className="grid-container">
+              {userFavorites[0]?.favorites.map((favorite, index) => (
+                <div key={index} className="fav-loc-card">
+                  <Link className="details-link" to={`/favorites/${favorite._id}`}>
+                    <h1>{favorite.name}</h1>
+                    <h2>{favorite.locationData.weather.current.temp_f}°F</h2>
+                    <h2>Wind Speed: {favorite.locationData.weather.current.wind_mph} MPH</h2>
+                    <h2>Humidity: {favorite.locationData.weather.current.humidity}%</h2>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <h1>No Favorites Found</h1> // Updated to show a more accurate message when no favorites are found
+          )}
+        </>
+      )}
+    </>
+  );
 };
 
 export default Favorites;
